88121f547111fb6bac1a9d09ea5d0908b3fe08f4
griffinht <git@griffinht.com>
2023-10-28 14:40:58 -0400

    add dexie

---
+++ frontend/src/scripts/dexie.js
@@ -0,0 +1,4 @@
+import Dexie from "https://unpkg.com/dexie@latest/dist/modern/dexie.mjs";
+console.log(Dexie)
+
+export default "bruh";
--- frontend/src/scripts/main.js
+++ frontend/src/scripts/main.js
@@ -1,5 +1,6 @@
 import { Module } from "./module.js"
 import vue from "./vue.js"
+import dexie from "./dexie.js"
 
 new Module();
 console.log("hello world");
