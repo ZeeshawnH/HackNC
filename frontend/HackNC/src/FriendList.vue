af7087a510e2ba9027502917b01678ef0e3689a7
Zeeshawn Hasnain <zahasnai@ncsu.edu>
2023-10-28 16:56:15 -0400

    Packages added

---
+++ frontend/HackNC/src/FriendList.vue
@@ -0,0 +1,26 @@
+<!-- FriendList.vue -->
+<template>
+  <ul>
+    <li v-for="friend in friends" :key="friend.id">
+      {{ friend.name }}, {{ friend.age }}
+    </li>
+  </ul>
+</template>
+
+<script>
+  import { liveQuery } from "dexie";
+  import { useObservable } from "@vueuse/rxjs";
+  import { db } from "./db";
+
+  export default {
+    name: "FriendList",
+    setup() {
+      return {
+        db,
+        friends: useObservable(
+          liveQuery(() => db.friends.toArray())
+        ),
+      };
+    },
+  };
+</script>
